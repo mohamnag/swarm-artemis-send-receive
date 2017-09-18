968cab7fb0da2804f169a2af4f6b10e4579b3ccb
Mohammad Naghavi <mohamnag@gmail.com>
2017-09-18 11:22:30 +0200

    adding equivalent custom main for swarm config

---
+++ src/main/java/com/mohamnag/examples/CustomMain.java
@@ -0,0 +1,42 @@
+package com.mohamnag.examples;
+
+import org.wildfly.swarm.Swarm;
+import org.wildfly.swarm.messaging.MessagingFraction;
+import org.wildfly.swarm.spi.api.OutboundSocketBinding;
+
+public class CustomMain {
+
+    public static void main(String[] args) throws Exception {
+        Swarm swarm = new Swarm(args);
+
+        swarm
+                .outboundSocketBinding("standard-sockets",
+                        new OutboundSocketBinding("remote-activemq")
+                                .remoteHost("localhost")
+                                .remotePort(61616))
+
+                .fraction(new MessagingFraction()
+                        .defaultServer(server -> {
+
+                            server.remoteConnector("remote-activemq", connector -> {
+                                connector.socketBinding("remote-activemq");
+                            });
+
+                            server.pooledConnectionFactory("remote", factory -> {
+                                factory.connectors("remote-activemq");
+                                factory.entries("java:/jms/remote-mq");
+                                factory.user("yourUser");
+                                factory.password("yourPassword");
+                            });
+
+                            server.jmsTopic("some-events", topic -> {
+                                topic.entries("java:/jms/topic/some-events");
+                            });
+
+                        }))
+
+                .start()
+                .deploy();
+    }
+
+}
